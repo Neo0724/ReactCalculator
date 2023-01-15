ca42403c69cde1a5abe2d5e5229177b1441d54ab
github_Neo0724 <neoben007@gmail.com>
2023-01-15 18:11:57 +0800

    Updates

---
+++ precache-manifest.1dd3636d5487978bd14a0559d0b4d6e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60d5dd00827baac738f3",
+    "url": "/ReactCalculator/static/css/main.6d813029.chunk.css"
+  },
+  {
+    "revision": "60d5dd00827baac738f3",
+    "url": "/ReactCalculator/static/js/main.60d5dd00.chunk.js"
+  },
+  {
+    "revision": "ef642488b71e60be5155",
+    "url": "/ReactCalculator/static/js/1.ef642488.chunk.js"
+  },
+  {
+    "revision": "855df1d8b3b172117c20",
+    "url": "/ReactCalculator/static/js/runtime~main.855df1d8.js"
+  },
+  {
+    "revision": "05f44b22b8d37335c2e2b2694658146c",
+    "url": "/ReactCalculator/index.html"
+  }
+];
